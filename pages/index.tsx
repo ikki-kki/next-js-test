7d91609afea016446d851de14acfafd167ec7be7
성정민 <hellojeongmin@gmail.com>
2020-11-30 23:03:54 +0900

    스타일링 적용"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
-import { useEffect } from "react";
+//index에서는 global css 같은 글로벌 import를 할 수 없다.
+import styles from "./index.module.scss";
 
-export default function Home(props) {
-  useEffect(() => {
-    alert("hello");
-  }, []);
-  return <h1>Hello World</h1>;
+// function Heading(props) {
+//   return (
+//     <div>
+//       <h1 className={styles.red}>
+//         <span>I'am red</span>
+//         {props.heading}
+//       </h1>
+//     </div>
+//   );
+// }
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Here</h1>
+    </div>
+  );
 }

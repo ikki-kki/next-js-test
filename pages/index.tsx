9bb7659260232358c54a1fbcd2811b87c73e4657
성정민 <hellojeongmin@gmail.com>
2020-11-30 23:05:05 +0900

    스타일링 적용"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 //index에서는 global css 같은 글로벌 import를 할 수 없다.
 import styles from "./index.module.scss";
 
-// function Heading(props) {
-//   return (
-//     <div>
-//       <h1 className={styles.red}>
-//         <span>I'am red</span>
-//         {props.heading}
-//       </h1>
-//     </div>
-//   );
-// }
+function Heading(props) {
+  return (
+    <div>
+      <h1 className={styles.red}>
+        <span>I'am red</span>
+        {props.heading}
+      </h1>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
